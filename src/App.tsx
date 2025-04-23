4f5a939bbd8c656c5e4c929dcd930287f4f03ae5
Edward Timmer <edtimmer@gmail.com>
2025-04-23 19:43:36 -0400

    add a second group of raiders

--- src/App.tsx
+++ src/App.tsx
@@ -80,16 +80,33 @@ function App() {
         <MilSatContainer>
           <Canvas gl={{ antialias: true }}>
             <PerspectiveCamera makeDefault fov={20} position={[0, 0, 20]} />
-            <directionalLight position={[0, 0, -2]} intensity={1} />
+            {/* <directionalLight position={[0, 0, -2]} intensity={1} />
             <directionalLight position={[0, 5, -2]} intensity={1} />
             <directionalLight position={[0, -5, -2]} intensity={1} />
 
             <directionalLight position={[0, 0, 20]} intensity={1} />
             
-            <directionalLight position={[0, -5, -5]} intensity={1} color={'#f9d87b'} />
+            <directionalLight position={[0, -5, -5]} intensity={1} color={'#f9d87b'} /> */}
             {!isMobile && <OrbitControls enableDamping enableZoom={false} />}
             {/* <MilSatGroup /> */}
-            <RaiderGroup />
+            <RaiderGroup 
+              position={[-0.5, 3.5, -12]}
+              rotation={[0.6, 0, -0.3]}
+              rollSpeed={0.6}
+              speed={0.27}
+              raiderOnePosition={[-15, 0, 0]}
+              raiderTwoPosition={[-13, 0, 2]}
+              raiderRotation={[-Math.PI / 2, -Math.PI / 2 + 2, Math.PI / 2]}
+            />
+            <RaiderGroup
+              position={[-12, 0, 15]}
+              rotation={[0.4, 0, 0]}
+              rollSpeed={1.2}
+              speed={0.27}
+              raiderOnePosition={[-13, 0, 0]}
+              raiderTwoPosition={[-11, 0, 2]}
+              raiderRotation={[-Math.PI / 2, 0, Math.PI / 2]}
+            />
             {/* <Raider position={[0, 0, 0]} rotation={new THREE.Euler(-Math.PI / 2, -Math.PI /2, Math.PI / 2)} scale={0.18} /> */}
             <Environment preset="warehouse" />
           </Canvas>
