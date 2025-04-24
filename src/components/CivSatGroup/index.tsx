7baaafa1df4c9da3e96973645569ff755e9fe8e1
Edward Timmer <edtimmer@gmail.com>
2025-04-24 11:28:56 -0400

    clean up

--- src/components/CivSatGroup/index.tsx
+++ src/components/CivSatGroup/index.tsx
@@ -5,13 +5,7 @@ import * as THREE from 'three'
 import Satellite from '../Satellite';
 
 function CivSatGroup() {
-  const groupOneRef = useRef<Group>(null);
-  const groupTwoRef = useRef<Group>(null);
-
   const groupRef = useRef<Group>(null);
-
-  const civSatOneRef = useRef<THREE.Mesh>(null);
-  const civSatTwoRef = useRef<THREE.Mesh>(null);
 
   useFrame((_, delta) => {
     if (groupRef.current) {   
@@ -20,31 +14,22 @@ function CivSatGroup() {
     
   });
 
-  useFrame((_, delta) => {
-    if (groupTwoRef.current) {
-      groupTwoRef.current.rotation.y -= delta * 0.1;
-    }    
-  });
-
   return (
     <group ref={groupRef} position={[3.5, 3, -8]} scale={[1, 1, 1]} rotation={[0.6, 0, 0]}>
-      {/* <group position={[-0.5, 0, 0]} scale={[1, 1, 1]} ref={groupOneRef} rotation={[0.4, 0, 0.2]}> */}
-        <Satellite
-          position={[9.5, 0, 0]}
-          rotation={new THREE.Euler(Math.PI / 2, 0.2, 0)}
-          scale={0.07}
-          offset={[-10, -5, 0]}
-          type={'civilian1'}
-        />
+      <Satellite
+        position={[9.5, 0, 0]}
+        rotation={new THREE.Euler(Math.PI / 2, 0.2, 0)}
+        scale={0.07}
+        offset={[-10, -5, 0]}
+        type={'civilian1'}
+      />
 
-      {/* <group position={[-2, 0, -5]} scale={[1, 1, 1]} ref={groupTwoRef} rotation={[0.4, 0, -0.4]}> */}
-        <Satellite
-          position={[-9.5, 0, 0]}
-          rotation={new THREE.Euler(-Math.PI / 2, Math.PI / 2 + 0.2, Math.PI)}
-          scale={0.08} offset={[-10, -5, 0]}
-          type={'civilian2'}
-        />
-      {/* </group>       */}
+      <Satellite
+        position={[-9.5, 0, 0]}
+        rotation={new THREE.Euler(-Math.PI / 2, Math.PI / 2 + 0.2, Math.PI)}
+        scale={0.08} offset={[-10, -5, 0]}
+        type={'civilian2'}
+      />
     </group>    
   );
 }
